b1e28ccf423090fff0f02d4900a22b484171db94
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-10-27 13:56:23 +0500

    chnages

--- app/api/products/index.tsx
+++ app/api/products/index.tsx
@@ -1,31 +1,31 @@
 import { createProduct, deleteProduct, getAllProducts } from "@/prisma/pruduct";
 
-export default async function handler(req : any, res : any) {
-    try {
-        switch (req.method) {
-            case "POST": {
-                const { image, title, price, category } = req.body;
+export default async function handler(req: any, res: any) {
+  try {
+    switch (req.method) {
+      case "POST": {
+        const { image, title, price, category } = req.body;
 
-                const new_product = await createProduct(image, title, price, category);
+        const new_product = await createProduct(image, title, price, category);
 
-                return res.status(201).json(new_product);
-            }
-            case "GET": {
+        return res.status(201).json(new_product);
+      }
+      case "GET": {
+        const products = await getAllProducts();
 
-                const products = await getAllProducts()
+        return res.status(200).json(products);
+      }
+      case "DELETE": {
+        const { id } = req.query;
 
-                return res.status(200).json(products);
-            }
-            case "DELETE": {
+        await deleteProduct(id);
 
-                const { id } = req.query
-
-                await deleteProduct(id)
-
-                return res.status(200).json({message: "Product deleted successfully"});
-            }
-        }
-    } catch (error) {
-        console.log(error);
+        return res
+          .status(200)
+          .json({ message: "Product deleted successfully" });
+      }
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
