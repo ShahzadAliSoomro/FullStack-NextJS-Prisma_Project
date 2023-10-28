657182bc0238a5f3f6840ccd94bc02024d8959ac
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-10-28 15:51:24 +0500

    changes

--- app/api/products/index.tsx
+++ app/api/products/index.tsx
@@ -1,31 +1,43 @@
-import { createProduct, deleteProduct, getAllProducts } from "@/prisma/pruduct";
 
-export default async function handler(req: any, res: any) {
-  try {
-    switch (req.method) {
-      case "POST": {
-        const { image, title, price, category } = req.body;
-
-        const new_product = await createProduct(image, title, price, category);
-
-        return res.status(201).json(new_product);
-      }
-      case "GET": {
-        const products = await getAllProducts();
-
-        return res.status(200).json(products);
-      }
-      case "DELETE": {
-        const { id } = req.query;
-
-        await deleteProduct(id);
-
-        return res
-          .status(200)
-          .json({ message: "Product deleted successfully" });
-      }
+export default function handler(req: any, res: any) {
+    console.log(req.method);
+     <h1>Hello</h1>
+    if(req.method =='GET'){
+        res.status(200).json({message: "Hello"});
+    }else {
+        res.status(200).json({message: "Method not allowed"});
     }
-  } catch (error) {
-    console.log(error);
-  }
 }
+
+// import { createProduct, deleteProduct, getAllProducts } from "@/prisma/pruduct";
+
+// export default async function handler(req: any, res: any) {
+//     console.log(req.method);
+//   try {
+//     switch (req.method) {
+//       case "POST": {
+//         const { image, title, price, category } = req.body;
+
+//         const new_product = await createProduct(image, title, price, category);
+
+//         return res.status(201).json(new_product);
+//       }
+//       case "GET": {
+//         const products = await getAllProducts();
+
+//         return res.status(200).json(products);
+//       }
+//       case "DELETE": {
+//         const { id } = req.query;
+
+//         await deleteProduct(id);
+
+//         return res
+//           .status(200)
+//           .json({ message: "Product deleted successfully" });
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
