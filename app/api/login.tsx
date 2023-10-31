ed15f1e057673a99bb0e04dcfc5f153ab9785865
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-10-31 19:44:03 +0500

    changes

--- app/api/login.tsx
+++ app/api/login.tsx
@@ -1,9 +1,9 @@
 export default function handler(req: any, res: any) {
-    console.log(req.method);
-     <h1>Hello</h1>
-    if(req.method ==='GET'){
-        res.status(200).json({message: "Hello"});
-    }else {
-        res.status(200).json({message: "Method not allowed"});
-    }
+  console.log(req.method);
+  <h1>Hello</h1>;
+  if (req.method === "GET") {
+    res.status(200).json({ message: "Hello" });
+  } else {
+    res.status(200).json({ message: "Method not allowed" });
+  }
 }
--- app/api/products/index.tsx
+++ app/api/products/index.tsx
@@ -1,12 +1,11 @@
-
 export default function handler(req: any, res: any) {
-    console.log(req.method);
-     <h1>Hello</h1>
-    if(req.method =='GET'){
-        res.status(200).json({message: "Hello"});
-    }else {
-        res.status(200).json({message: "Method not allowed"});
-    }
+  console.log(req.method);
+  <h1>Hello</h1>;
+  if (req.method == "GET") {
+    res.status(200).json({ message: "Hello" });
+  } else {
+    res.status(200).json({ message: "Method not allowed" });
+  }
 }
 
 // import { createProduct, deleteProduct, getAllProducts } from "@/prisma/pruduct";
