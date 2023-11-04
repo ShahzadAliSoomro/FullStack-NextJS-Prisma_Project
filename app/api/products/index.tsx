42500888329be66ccd2462bb8d792f731f7ca3a1
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-11-04 18:53:50 +0500

    changes

--- app/api/products/index.tsx
+++ app/api/products/index.tsx
@@ -1,6 +1,6 @@
 export default function handler(req: any, res: any) {
   console.log(req.method);
-  
+  <h1>Hello</h1>
   if (req.method == "GET") {
     res.status(200).json({ message: "Hello" });
   } else {
