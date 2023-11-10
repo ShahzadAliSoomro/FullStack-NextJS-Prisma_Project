2b2c4031c4c462ee3a2b3bf451f86a79e8f1aef6
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-11-10 23:44:47 +0500

    changes

---
+++ app/components/Arrays/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function Arrays() {
+  let marks_class_12 = [91, 82, 63, 84, false, "Not Present"];
+console.log(marks_class_12);
+  return (
+    <div>Arrays</div>
+  )
+}
